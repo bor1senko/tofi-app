7f6b804e2dfe40a517cda76ead37474c1d8e178e
ByMyTry <anton.taleckij@gmail.com>
2017-12-07 01:22:54 +0300

    width of slider full width of screen

--- services/frontend/app/src/core/controllers/landing.controller.js
+++ services/frontend/app/src/core/controllers/landing.controller.js
@@ -3,22 +3,25 @@ angular.module("ui.bootstrap.core", ['ngAnimate', 'ngSanitize', 'ui.bootstrap'])
 module.controller('HomeController', HomeController);
 
 function HomeController($scope) {
-    $scope.myInterval = 5000;
+    $scope.myInterval = 3500;
     $scope.noWrapSlides = false;
     $scope.active = 0;
     var slides = $scope.slides = [];
     var currIndex = 0;
+
     var slideItems = [
         {video: '/upload/iblock/d79/insync.mp4', text:'Улыбающаяся кукла'},
         {video: '/upload/iblock/5d8/banner_rentalcars.mp4', text:'Продал хату - купи тачку'},
         {video: '/upload/iblock/c98/booking.mp4', text:'Какая интересная карта'},
-        {video: '/upload/iblock/2f7/credits.mp4', text:'Слоууумооо'}
+        {video: '/upload/iblock/2f7/credits.mp4', text:'Слоууумооо', styleHeight: 'height:52em;'}
     ]
 
     $scope.addSlide = function(slideItem) {
+        if(slideItem.styleHeight === undefined) slideItem.styleHeight = 'height:40em;';
         slides.push({
           video: 'https://www.alfabank.by/' + slideItem.video,
           text: slideItem.text,
+          styleHeight: slideItem.styleHeight,
           id: currIndex++
         });
     };
