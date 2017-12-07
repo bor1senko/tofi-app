4d18334db3ac280e03c25e45529c1287adb01d8a
Ivan Borisenko <i.borisenko97@gmail.com>
2017-12-07 10:39:25 +0300

    landing page

--- services/frontend/app/src/core/controllers/landing.controller.js
+++ services/frontend/app/src/core/controllers/landing.controller.js
@@ -3,21 +3,19 @@ angular.module("ui.bootstrap.core", ['ngAnimate', 'ngSanitize', 'ui.bootstrap'])
 module.controller('HomeController', HomeController);
 
 function HomeController($scope) {
-    $scope.myInterval = 5000;
+    $scope.myInterval = 1000;
     $scope.noWrapSlides = false;
     $scope.active = 0;
     var slides = $scope.slides = [];
     var currIndex = 0;
     var slideItems = [
-        {video: '/upload/iblock/d79/insync.mp4', text:'Улыбающаяся кукла'},
-        {video: '/upload/iblock/5d8/banner_rentalcars.mp4', text:'Продал хату - купи тачку'},
-        {video: '/upload/iblock/c98/booking.mp4', text:'Какая интересная карта'},
-        {video: '/upload/iblock/2f7/credits.mp4', text:'Слоууумооо'}
+        {video: 'assets/img/email.icon.png', text:'telegram оповещения'},
+        {video: 'http://activeinvestor.pro/wp-content/uploads/2014/08/kak-vybrat-vklad.jpg', text:'вклады'}
     ]
 
     $scope.addSlide = function(slideItem) {
         slides.push({
-          video: 'https://www.alfabank.by/' + slideItem.video,
+          video:  slideItem.video,
           text: slideItem.text,
           id: currIndex++
         });
